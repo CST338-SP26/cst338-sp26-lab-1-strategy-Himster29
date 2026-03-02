328817dca4bde572779b0f5b3adc647d103198df
Himansu Yapa <hyapa@csumb.edu>
2026-03-02 11:35:15 -0800

    added code to flesh it out

---
+++ src/main/java/Kobold.java
@@ -0,0 +1,11 @@
+public class Kobold extends Monster {
+
+    public Kobold(Integer hp, Integer xp, Integer maxHP, HashMap<String, Integer> items) {
+        super(hp, xp, maxHP, items);
+    }
+
+    @Override
+    public String toString() {
+        return ""; // placeholder
+    }
+}
