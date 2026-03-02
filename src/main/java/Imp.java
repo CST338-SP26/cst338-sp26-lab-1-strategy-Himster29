e15ea2763dcd7acc87d33477e664002d51d9cad3
Himansu Yapa <hyapa@csumb.edu>
2026-03-02 11:30:43 -0800

    imp fleshed out

---
+++ src/main/java/Imp.java
@@ -0,0 +1,11 @@
+public class Imp extends Monster {
+
+    public Imp(Integer hp, Integer xp, Integer maxHP, HashMap<String, Integer> items) {
+        super(hp, xp, maxHP, items);
+    }
+
+    @Override
+    public String toString() {
+        return ""; // placeholder
+    }
+}
